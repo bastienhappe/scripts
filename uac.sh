f573f51e4ee7c6aaa810f8e2ea2835c6e6ac30a8
bastienhappe <bastien.happe@gmail.com>
2013-02-15 10:42:03 +0100

    third commit

---
+++ uac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/usr/local/bin/sipp 212.85.130.90:5060 -s 973330001 -t u1 -ci 212.85.130.11 -m 1 -cid_str 322857383 -trace_logs -trace_msg -d 4000
+
+#/usr/local/bin/sipp 212.85.130.91 -sf branchc.xml -s 0973330001 -trace_logs -trace_msg
+
