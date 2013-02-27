0cc083824afa9249ffe9a6b7bcfd5d1a6fad906f
bastienhappe <bastien.happe@gmail.com>
2013-02-27 14:49:45 +0100

    clearing

--- tests/submit.sh
+++ tests/submit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 find . -maxdepth 1 -type d  -name 'test*'  | \
-=======
 find . -type d -name 'test*' -maxdepth 0 -print 0 | \
->>>>>>> a629f690f8119b2a3b8d9954b76e6d8f762d64ef
   xargs -0 ./submit-test.sh
