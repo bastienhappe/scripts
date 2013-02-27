4d09bccd86bb6452755c2c9fe3ff4e7172bb971f
bastienhappe <bastien.happe@gmail.com>
2013-02-27 15:02:32 +0100

    clearing

--- submit.sh
+++ submit.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-find . -maxdepth 1 -type d  -name 'test*'  | \
-=======
-find . -type d -name 'test*' -maxdepth 0 -print 0 | \
->>>>>>> a629f690f8119b2a3b8d9954b76e6d8f762d64ef
+find . -type d -name 'test*' -maxdepth 1  | \
   xargs -0 ./submit-test.sh
