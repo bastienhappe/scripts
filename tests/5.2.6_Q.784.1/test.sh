d283a2556880e9f6b4e68e7ef17814f67f08e666
bastienhappe <bastien.happe@gmail.com>
2013-03-19 15:49:37 +0100

    new test

---
+++ tests/5.2.6_Q.784.1/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script variables
+STAMP=`date -u '+%F-%T'`
+TIME=${TIME:-620}
+RHGW=root@212.85.130.91
+
+
+mkdir $STAMP
+cd $STAMP
+
+# MTP3 capture
+	 ( ssh $RHGW "(sleep $TIME; echo) | wanpipemon -i w1g1 -pcap -pcap_file w1g1-mtp3.pcap -mtp2-msu -prot MTP2 -full -systime -c trd"; scp  $RHGW:w1g1-mtp3.pcap .; )&
+sleep 5
+echo block
+# Blocking circuit
+ssh root@212.85.130.90 "echo -e \"control isup1/ISUP block circuit=1\nquit\" | nc localhost 5038"
+echo unblock
+#Unblocking circuit
+ssh root@212.85.130.90 "echo -e \"control isup1/ISUP unblock circuit=1\nquit\" | nc localhost 5038"
