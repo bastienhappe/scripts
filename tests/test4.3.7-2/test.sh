fe2450218c739108dd06ee398bc15a24c86ffceb
bastienhappe <bastien.happe@gmail.com>
2013-04-05 15:15:55 +0200

    User part test

---
+++ tests/test4.3.7-2/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script variables
+STAMP=`date -u '+%F-%T'`
+TIME=${TIME:-620}
+RHGW=root@212.85.130.91
+
+sleep 5
+
+# MTP3 capture
+#	 ( ssh $RHGW "(sleep $TIME; echo) | wanpipemon -i w1g1 -pcap -pcap_file w1g1-mtp3.pcap -mtp2-msu -prot MTP2 -full -systime -c trd"; scp  $RHGW:w1g1-mtp3.pcap .; )&
+ssh root@212.85.130.90 "echo -e \"control isup1/ISUP parttest circuit=1\nquit\" | nc localhost 5038"
